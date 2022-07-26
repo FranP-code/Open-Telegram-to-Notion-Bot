287622822afa9c9394ac2aa0a4a038c875cba793
Francisco Pessano <franpessano1@gmail.com>
2022-07-26 11:46:57 -0300

    help command bugfix

--- src/app/commands/help.js
+++ src/app/commands/help.js
@@ -1,14 +1,14 @@
 const reply = require("../../scripts/reply")
 
 function help(ctx) {
-    reply(ctx
-        `
+	reply(ctx,
+		`
 • The repository of this bot on <strong>Github</strong>:
   https://github.com/FranP-code/Telegram-to-Notion-Bot
 
 • The <strong>website</strong> of this project:
   https://telegram-to-notion-bot.netlify.app
-        `, {parse_mode: "HTML"})
+        `, { parse_mode: "HTML" })
 }
 
 module.exports = help
