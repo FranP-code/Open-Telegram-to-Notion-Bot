ce26101542f7ce20de5b3ab51db8bfd72d554445
Francisco Pessano <franpessano1@gmail.com>
2024-10-27 17:06:44 -0300

    feat: feedback form

--- src/app/commands/feedback.ts
+++ src/app/commands/feedback.ts
@@ -2,5 +2,9 @@ import { BotContext } from "../types";
 import reply from '../../scripts/reply';
 
 export default function feedback(ctx: BotContext) {
-  reply(ctx, 'Do you want to give me feedback?\n\n<strong>Message me!</strong>\n<strong>@frankaP</strong>', { parse_mode: 'HTML' });
+  if (!process.env.FEEDBACK_FORM_URL) {
+    return reply(ctx, 'Sorry, there is no feedback form available at the moment.');
+  } else {
+    return reply(ctx, 'Do you want to give me feedback?\n\n<a href="' + process.env.FEEDBACK_FORM_URL + '">Click here to fill a little form to contact me!</a>', { parse_mode: 'HTML' });
+  }
 }
