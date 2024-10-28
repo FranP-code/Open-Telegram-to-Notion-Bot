6c62aaa5fd00d2d6bd6e6b1e774551b5808b2793
Francisco Pessano <franpessano1@gmail.com>
2024-10-27 21:37:12 -0300

    fix: improve feedback message formatting

--- src/app/commands/feedback.ts
+++ src/app/commands/feedback.ts
@@ -10,9 +10,7 @@ export default function feedback(ctx: BotContext) {
   } else {
     return reply(
       ctx,
-      'Do you want to give me feedback?\n\n<a href="' +
-        process.env.FEEDBACK_FORM_URL +
-        '">Click here to fill a little form to contact me!</a>',
+      `Do you want to give me feedback?\n\n<a href="${process.env.FEEDBACK_FORM_URL}">Click here to fill a little form to contact me!</a>`,
       { parse_mode: "HTML" },
     );
   }
