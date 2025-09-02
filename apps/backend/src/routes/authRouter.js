3223812b5dc4a4ed0257430bbc0508f6a41d7409
Francisco Pessano <franpessano1@gmail.com>
2025-09-02 12:55:02 -0300

    fix: simplify authorization setup in authRouter by consolidating auth object

--- apps/backend/src/routes/authRouter.js
+++ apps/backend/src/routes/authRouter.js
@@ -14,34 +14,18 @@ router.post('/', async (req, res) => {
         redirect_uri: 'https://open-telegram-to-notion.vercel.app/auth'
       }
 
-      const auth = {
-				Authorization:
-					"Basic " +
-					Buffer.from(
-						`${process.env.NOTION_INTEGRATION_ID}:${process.env.NOTION_INTEGRATION_SECRET}`
-					).toString("base64"),
-				Authorization2: `Basic ${Buffer.from(
-					`${process.env.NOTION_INTEGRATION_ID}:${process.env.NOTION_INTEGRATION_SECRET}`
-				).toString("base64")}`,
-			};
-
-      console.log(auth)
-
       const response = await axios({
 				method: "POST",
 				url: "https://api.notion.com/v1/oauth/token",
 				data: reqData,
-				// auth: {
-				//   username: Buffer.from(
-				//     process.env.NOTION_INTEGRATION_ID.toString('base64')
-				//   ),
-				//   password: Buffer.from(
-				//     process.env.NOTION_INTEGRATION_SECRET.toString('base64')
-				//   )
-				// } // THANK YOU https://stackoverflow.com/questions/67534080/notion-api-invalid-client-oauth-integration/68699544#68699544?newreg=949504cf865c4a52b2c0ce7afe936c9b
-				Authorization: `Basic ${Buffer.from(
-					`${process.env.NOTION_INTEGRATION_ID}:${process.env.NOTION_INTEGRATION_SECRET}`
-				).toString("base64")}`,
+				auth: {
+				  username: Buffer.from(
+				    process.env.NOTION_INTEGRATION_ID.toString('base64')
+				  ),
+				  password: Buffer.from(
+				    process.env.NOTION_INTEGRATION_SECRET.toString('base64')
+				  )
+				} // THANK YOU https://stackoverflow.com/questions/67534080/notion-api-invalid-client-oauth-integration/68699544#68699544?newreg=949504cf865c4a52b2c0ce7afe936c9b
 			});
 
       console.log(response.status) // 400 in positive case
